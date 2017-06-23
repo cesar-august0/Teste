8bb5a795a19be82b30969d9dfb6ac33d0e52a390
felipeam <felipeam@ttraartt>
2017-06-22 21:46:19 -0300

    oi

---
+++ src/br/com/ueg/doacao/telacadastro/Cadastro_Controller.java
@@ -0,0 +1,12 @@
+package br.com.ueg.doacao.telacadastro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Cadastro_Controller{
+	
+	
+
+}
